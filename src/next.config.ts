cd7dcf8c2ef9f68fab46c58c9d140601ade531a9
Tarık BAĞRIYANIK <tbagriyanik@gmail.com>
2025-09-08 18:16:00 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,31 @@
+import type {NextConfig} from 'next';
+
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
