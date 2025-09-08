b6c3660f86f110e4f48ce7d94cfee29177e75e14
Tarık BAĞRIYANIK <tbagriyanik@gmail.com>
2025-09-08 19:25:15 +0000

    A `locale` is expected to be returned from `getRequestConfig`, but none

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,13 @@
+import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
  
-export default getRequestConfig(async ({locale}) => ({
-  messages: (await import(`../messages/${locale}.json`)).default
-}));
+const locales = ['en', 'tr'];
+ 
+export default getRequestConfig(async ({locale}) => {
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
+    messages: (await import(`../messages/${locale}.json`)).default,
+    locale,
+  };
+});
